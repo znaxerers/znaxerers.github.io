38237cecef15f2d9ac36c36e256507d3726d398e
Zim <104741739+znaxerers@users.noreply.github.com>
2024-11-03 21:49:16 +0000

    version 1
    first version of the website

---
+++ script.js
@@ -0,0 +1,33 @@
+$(function(){
+  var x = 0;
+  var y = 0;
+  setInterval(function(){
+    x -=1;
+    y -=1;
+    $('#bg').css('background-position', x + 'px ' + y +'px');
+  }, 30);
+});
+
+let slideIndex = [1,1,1,1];
+/* Class the members of each slideshow group with different CSS classes */
+let slideId = ["mySlides1", "mySlides2", "mySlides3", "mySlides4"]
+showSlides(1, 0);
+showSlides(1, 1);
+showSlides(1, 2);
+showSlides(1, 3);
+
+// Next/previous controls
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[slideIndex[no]-1].style.display = "block";
+}
